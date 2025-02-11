aac5a941d95d0defb1e0f03673789272f241c82a
Neil Swart <neilswart3@gmail.com>
2025-02-11 09:41:31 +0100

    add palette

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import { useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import "./App.css";
-import { Button, Input, Stack, Image } from "@chakra-ui/react";
+import { Button, Input, Stack, Image, Box } from "@chakra-ui/react";
+import { prominent } from "color.js";
 
 function App() {
+  const [palette, setPalette] = useState<string[]>([]);
   const [imgObj, setImgObj] = useState<{
     imagePreview: string;
     imageFile: unknown;
@@ -20,6 +22,25 @@ function App() {
       imageFile: event.target.files![0],
     });
   };
+
+  const handleGetPalette = useCallback(async () => {
+    try {
+      const colors = await prominent(imgObj?.imagePreview as string, {
+        amount: 5,
+        format: "hex",
+      });
+
+      setPalette(colors as string[]);
+    } catch (error) {
+      console.log("error:", error);
+    }
+  }, [imgObj?.imagePreview]);
+
+  useEffect(() => {
+    if (imgObj?.imageFile) {
+      handleGetPalette();
+    }
+  }, [handleGetPalette, imgObj?.imageFile]);
 
   return (
     <Stack padding={4} gap={4} w="full">
@@ -50,12 +71,21 @@ function App() {
             <Image src={imgObj?.imagePreview} alt="img" h="100%" w="full" />
           ) : (
             <Stack alignItems="center" justifyContent="center" h="full">
-
-            <p>photo goes here</p>
+              <p>photo goes here</p>
             </Stack>
           )}
         </Stack>
       </Stack>
+
+      {palette.length > 0 && (
+        <Stack p={2} gap={2} backgroundColor="gray.100" borderRadius={8}>
+          {palette.map((color, index) => (
+            <Box backgroundColor={color} key={index} p={4} borderRadius={8}>
+              {color}
+            </Box>
+          ))}
+        </Stack>
+      )}
     </Stack>
   );
 }
