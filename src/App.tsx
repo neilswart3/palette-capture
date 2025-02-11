d6f63797120e8f0235bd6f7b35e7232ee6037b50
Neil Swart <neilswart3@gmail.com>
2025-02-11 11:47:46 +0100

    install

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { useCallback, useEffect, useRef, useState } from "react";
 import tinycolor from "tinycolor2";
 import "./App.css";
-import { Button, Input, Stack, Image, Tabs } from "@chakra-ui/react";
+import { Button, Input, Stack, Image, Tabs, Icon, Box } from "@chakra-ui/react";
 import { average, prominent } from "color.js";
 import Case from "case";
+import { MdClose, MdDownload } from "react-icons/md";
 
 interface Palette {
   prominent: string[];
@@ -11,6 +12,8 @@ interface Palette {
 }
 
 function App() {
+  const [hasApp, setHasApp] = useState(false);
+  const [showInstall, setShowInstall] = useState(true);
   const [palette, setPalette] = useState<Partial<Palette>>({});
   const [imgObj, setImgObj] = useState<{
     imagePreview: string;
@@ -18,6 +21,7 @@ function App() {
   } | null>(null);
 
   const handleFileInput = useRef<HTMLInputElement>(null);
+  const installPrompt = useRef<Event | null>(null);
 
   const handleClick = () => {
     handleFileInput.current?.click();
@@ -26,7 +30,7 @@ function App() {
   const handleClear = () => {
     setImgObj(null);
     setPalette({});
-    handleFileInput.current!.value = undefined as unknown as string; 
+    handleFileInput.current!.value = undefined as unknown as string;
   };
 
   const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -62,14 +66,78 @@ function App() {
     }
   }, [imgObj?.imagePreview]);
 
+  const handleInstallApp = useCallback(async () => {
+    try {
+      if (!installPrompt.current) return;
+
+      const result = await (
+        installPrompt.current as Event & { prompt: () => Promise<unknown> }
+      ).prompt();
+
+      if (result) {
+        installPrompt.current = null;
+        setHasApp(true);
+        setShowInstall(false);
+      }
+    } catch (error) {
+        console.log('error:', error)
+    }
+  }, []);
+
   useEffect(() => {
     if (imgObj?.imageFile) {
       handleGetPalette();
     }
   }, [handleGetPalette, imgObj?.imageFile]);
 
+  useEffect(() => {
+    const handleBeforeInstallPrompt = (event: Event) => {
+      event.preventDefault();
+      installPrompt.current = event;
+    };
+
+    window.addEventListener("beforeinstallprompt", handleBeforeInstallPrompt);
+
+    return () => {
+      window.removeEventListener(
+        "beforeinstallprompt",
+        handleBeforeInstallPrompt
+      );
+    };
+  }, []);
+
   return (
     <Stack padding={4} gap={4} w="full">
+      {showInstall && (
+        <Stack
+          alignItems="center"
+          backgroundColor={"gray.600"}
+          borderRadius={6}
+          p={4}
+          gap={4}
+        >
+          <Box>Install the App?</Box>
+          <Stack flexDir="row" gap={4}>
+            <Button onClick={handleInstallApp}>
+              <Icon>
+                <MdDownload />
+              </Icon>
+              Install App
+            </Button>
+            <Button
+              variant="outline"
+              borderColor="white"
+              onClick={() => setShowInstall(false)}
+            >
+              <Icon>
+                <MdClose />
+              </Icon>
+              Dismiss
+            </Button>
+          </Stack>
+        </Stack>
+      )}
+
       <Input
         display="none"
         ref={handleFileInput}
