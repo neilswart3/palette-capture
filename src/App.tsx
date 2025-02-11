5f5bcc41907fab2fa9e2405fd9a11ed85065490e
Neil Swart <neilswart3@gmail.com>
2025-02-11 08:49:19 +0100

    test button another way

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,44 @@
+import { useRef, useState } from "react";
 import "./App.css";
 import { Button, Stack } from "@chakra-ui/react";
 
 function App() {
-  const handleOpenCamera = () => {
-    navigator.mediaDevices.getUserMedia({ video: true });
+  const [imgObj, setImgObj] = useState<{imagePreview: string; imageFile: unknown} | null>(null);
+
+  const handleFileInput = useRef<HTMLInputElement>(null);
+
+  const handleClick = () => {
+    handleFileInput.current?.click();
+  };
+
+//   const handleOpenCamera = () => {
+//     navigator.mediaDevices.getUserMedia({ video: true });
+//   };
+
+  const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setImgObj({
+      imagePreview: URL.createObjectURL(event.target.files![0]),
+      imageFile: event.target.files![0],
+    });
   };
 
   return (
     <Stack padding={4} gap={8}>
-      <Button as={Stack} onClick={handleOpenCamera}>
-        Add photo
-      </Button>
+      <div>
+        <Button as={Stack} onClick={handleClick}>
+          Add photo
+        </Button>
+        <label>
+          <input
+            type="file"
+            accept="image/*"
+            capture="environment"
+            onChange={handleImageChange}
+          />
+        </label>
+      </div>
 
-      <input type="file" accept="image/*" capture="environment" />
+      {imgObj && <img src={imgObj?.imagePreview} alt="img" />}
     </Stack>
   );
 }
