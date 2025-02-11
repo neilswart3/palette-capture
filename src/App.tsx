ce10e49ad0831d0a0d0182d145c01eff1738fcbe
Neil Swart <neilswart3@gmail.com>
2025-02-11 08:37:43 +0100

    add buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import appLogo from '/favicon.svg'
-import PWABadge from './PWABadge.tsx'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import appLogo from "/favicon.svg";
+import PWABadge from "./PWABadge.tsx";
+import "./App.css";
+import { Button, Stack } from "@chakra-ui/react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const handleOpenCamera = () => {
+    navigator.mediaDevices.getUserMedia({ video: true });
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={appLogo} className="logo" alt="Palette Capture logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Palette Capture</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <PWABadge />
-    </>
-  )
+    <Stack padding={4} gap={8}>
+      <Button as={Stack} onClick={handleOpenCamera}>
+        Add photo
+      </Button>
+
+      <input type="file" accept="image/*" capture="environment" />
+    </Stack>
+  );
 }
 
-export default App
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { VitePWA } from "vite-plugin-pwa";
 import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    tsconfigPaths(),
     VitePWA({
       registerType: "autoUpdate",
       injectRegister: false,
@@ -20,6 +22,32 @@ export default defineConfig({
         short_name: "palette-capture",
         description: "An app to generate a palette of colors from a photo",
         theme_color: "#171717",
+        icons: [
+          {
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "favicon",
+          },
+          {
+            src: "/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "favicon",
+          },
+          {
+            src: "/apple-touch-icon.png",
+            sizes: "180x180",
+            type: "image/png",
+            purpose: "apple touch icon",
+          },
+          {
+            src: "/maskable_icon.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
       },
 
       workbox: {
