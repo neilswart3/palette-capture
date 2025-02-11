b75c03da5b6e3cedbb0d2a3918d12c75b2932f06
Neil Swart <neilswart3@gmail.com>
2025-02-11 09:09:02 +0100

    update styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useRef, useState } from "react";
 import "./App.css";
-import { Button, Stack } from "@chakra-ui/react";
+import { Button, Input, Stack, Image } from "@chakra-ui/react";
 
 function App() {
   const [imgObj, setImgObj] = useState<{
@@ -14,10 +14,6 @@ function App() {
     handleFileInput.current?.click();
   };
 
-  //   const handleOpenCamera = () => {
-  //     navigator.mediaDevices.getUserMedia({ video: true });
-  //   };
-
   const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setImgObj({
       imagePreview: URL.createObjectURL(event.target.files![0]),
@@ -26,23 +22,40 @@ function App() {
   };
 
   return (
-    <Stack padding={4} gap={8}>
+    <Stack padding={4} gap={4} w="full">
       <div>
-        <Button as={Stack} onClick={handleClick}>
+        <Button as={Stack} onClick={handleClick} w="full">
           Add photo
         </Button>
-        <label>
-          <input
-            ref={handleFileInput}
-            type="file"
-            accept="image/*"
-            capture="environment"
-            onChange={handleImageChange}
-          />
-        </label>
+        <Input
+          display="none"
+          ref={handleFileInput}
+          type="file"
+          accept="image/*"
+          capture="environment"
+          onChange={handleImageChange}
+        />
       </div>
 
-      {imgObj && <img src={imgObj?.imagePreview} alt="img" />}
+      <Stack
+        alignItems="center"
+        justifyContent="center"
+        padding={4}
+        border="1px dashed orange"
+        borderRadius={8}
+        h="30vh"
+      >
+        <Stack objectFit="contain" h="full">
+          {imgObj ? (
+            <Image src={imgObj?.imagePreview} alt="img" h="100%" w="full" />
+          ) : (
+            <Stack alignItems="center" justifyContent="center" h="full">
+
+            <p>photo goes here</p>
+            </Stack>
+          )}
+        </Stack>
+      </Stack>
     </Stack>
   );
 }
